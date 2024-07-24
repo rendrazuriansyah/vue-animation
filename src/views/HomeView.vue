dfa45f2c9f3d30a4243305b91a3d9c16ed028057
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-24 14:26:48 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+	<main>
+		<div class="container">
+			<input type="text" autofocus />
+			<div class="card-list">Task 1</div>
+			<div class="card-list">Task 2</div>
+			<div class="card-list">Task 3</div>
+		</div>
+	</main>
+</template>
+
+<style scoped>
+/* * {
+	border: 1px solid red;
+} */
+
+.container {
+	max-width: 300px;
+	margin: 0 auto;
+}
+
+.container input {
+	width: 100%;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	padding: 10px;
+	margin-bottom: 20px;
+	box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+}
+
+.card-list {
+	width: 300px;
+	border-radius: 5px;
+	padding: 5px 10px;
+	margin-top: 10px;
+	box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+	text-align: center;
+	cursor: pointer;
+}
+</style>
